e1cfdb56711cb0c0df197173eedcb67aa3543879
Moamen Basel <momenbasel@gmail.com>
2018-12-07 18:45:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+*Do you think ₿itcoin value more than it's now?*
+<br/>
+![BTC logo](logo.jpg "Logo btc") 
+<br/>
+🛑Introducing:
+```
+  ____ ___ _____   ____ _____  _    ____  _     _____
+ | __ )_ _|_   _| / ___|_   _|/ \  | __ )| |   | ____|
+ |  _ \| |  | |   \___ \ | | / _ \ |  _ \| |   |  _|
+ | |_) | |  | |    ___) || |/ ___ \| |_) | |___| |___
+ |____/___| |_|   |____/ |_/_/   \_\____/|_____|_____|
+```
+
+A NodeJS trading bot that when BTC reaches to a value you expect or you want to trade with it automatically trade BTC with a stable coin like USDT (a coin which is literally linked with United States dollar ratio one-to-one) means for every USDT coin it equals one United States dollar.
+
+*I recommend to run it at virtual machine or Raspberry Pi as it saves the wallet private key at txt file which can be compressed.*
+
+just run it and leave it, it's that's simple!
+
+
+## Install 
+
+###### Prerequisites
+NodeJS
+NPM
+
+###### Dependencies
+* Bitcore
+* bitcore-explorers
+* inquirer
+* figlet 
+* chalk-pipe	
+
+
+
+[Download NodeJS](https://nodejs.org/en/download/)  && Setup it.
+
+`$ brew install node` for MacOS
+
+
+###### Installing 
+
+``` $ git clone https://github.com/momenbasel/bitstable.git 
+$ cd bistable 
+$ npm install 
+```
+
+###### Running 
+`$ node bistable `
+
+Send BTC to the address appears and wait until the transaction is confirmed then
+Enters:
+* Your [USDT wallet address](https://app.tether.to/app/#!/signup)
+* Amount of SATOSHI you want to trade with
+* The USD💵value you want to exchange Bitcoin with, when bitcoin reaches the value you enter
+
+Make sure you the message that appear is right then choose 
+* (YES I WANT TO MAKE THIS TRANSCATION!)
+
+
+**That's it, Happy trading! 💵💵**
+
+I will be happy if you buy me a coffee as *THIS WILL ENCOURAGE ME TO CONTINUE MONITOR/UPDATE THE SCRIPT*
+
+```
+BTC: 1AGyqezdgfCKUbHSpfhForTzxHuE3oaDzM
+BCH: qqq895mac5qhljp2mf6y9m44epur94se9y8hvqtwy4
+ETH: 0x3C911eed8339a4D93d7420CbE499F5c8bfEDb3D8
+ETC: 0x30b04737AAD2D88aCD27c934971B55336fe028A9
+LTC: MKmThaBysuN6xTkjWpDgseojm5mJzBqSPC
+Zcash: t1Kaj5ou7o3UWFZxeCYbD851Fzi37umZzdZ
+XMR: 48DFvQtx8grMtaR7ZZcjbb7jzkhmarinwb3V1TR1gmECjFHayp7DfVoL8WaQVkjwsvbeTn7gLaT8hSj3fKkh1eZ8BLpN1LY
+== MORE? ==
+Stellar: GC4Y3WWAR534CUF7AFMLTOSFGFE5Q4TJX5VR53VW43WZUNKCUMJUNZ7E
+BAT: 0xdDd445Cce7075CB4B64C2bc7d92c1bA7e7268A99
+ZRX: 0xD6349DcC6829D1A0161FEFea169D04a929C3F074
+```
---
+++ bitstable.js
@@ -0,0 +1,321 @@
+const fs = require('fs');
+const readline = require('readline');
+const https = require('https');
+var inquirer = require('inquirer');
+var chalkPipe = require('chalk-pipe');
+const bitcore = require('bitcore-lib');
+delete global._bitcore; // remove in production
+const explorers  = require('bitcore-explorers');
+const figlet = require('figlet');
+// it's recommended run this script on virtual machine.
+const privkey = bitcore.PrivateKey(); // important
+const publickey_uncompressed = privkey.toPublicKey();
+const publickey = publickey_uncompressed.toAddress().toString(); // PUBLIC KEY TO ADDRESS
+
+
+figlet('BIT STABLE', function(err, logo) {
+  if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+  }
+
+  console.log(logo)
+  console.log('Do you wanna to help? 1AGyqezdgfCKUbHSpfhForTzxHuE3oaDzM')
+  console.log('it\'s recommended run this script on virtual machine.\n\n\n\n\n')
+
+
+  if (fs.existsSync('bitcoinWallet.txt')) {
+      const rl = readline.createInterface({
+          input: fs.createReadStream('bitcoinWallet.txt'),
+          crlfDelay: Infinity
+      });
+
+      var lineCounter = 0;
+      var wantedLines = [];
+      rl.on('line', function(line) {
+          lineCounter++;
+          wantedLines.push(line);
+          if (lineCounter == 3) {
+              rl.close();
+          }
+      });
+      rl.on('close', function() {
+          Oldprivkey = wantedLines[1]; // important
+          Oldpublickey = wantedLines[2];
+          console.log(`your BTC wallet is: ${Oldpublickey} \n send BTC to it to start the tradebot 🤖`);
+          BTCRecived(Oldpublickey,Oldprivkey);
+      });
+
+
+  } else {
+      fs.writeFile("bitcoinWallet.txt", `${Date()} THIS FILE IS VERY SENSITIVE AND IMPORTANT DON'T SHARE/DELETE IT DOING THAT IS ON YOUR OWN RISK\n${privkey}\n${publickey}`, function(err) {
+          if (err) {
+              return console.log(err);
+          }
+          console.log(`Created Wallet for the script! \n SEND BTC TO ${publickey}`);
+          BTCRecived(publickey,privkey);
+      });
+  }
+
+
+  function BTCRecived(address,privatekey) {
+          https.get(`https://blockchain.info/q/addressbalance/${address}?confirmations=6` , (resp) => {
+              //change 'confirmations=6' to any number of confirmation to transcation confirmation you want but please note 6 is the min.
+          let balance = '';
+          resp.on('data', (chunk) => {
+              balance += chunk;
+          });
+          resp.on('end', () => {
+              if(balance != 0) {
+                  console.log(`Recived  ${balance * 0.00000001} ฿ `);
+                  var questions = [
+                      {
+                        type: 'input',
+                        name: 'usdt',
+                        message: "What's your USDT address?"
+                      },
+                      {
+                        type: 'input',
+                        name: 'Satoshiamount',
+                        message: "What's your BTC amount you want to trade with? (IN SATOSHI)",
+                        default: function() {
+                          return '3600000'; //min.
+                        }
+                        ,
+                        validate: value =>
+                        new Promise(resolve => {
+                          setTimeout(
+                            () =>
+                              resolve(
+                                (value.length && !Number.isNaN(Number(value))) ||
+                                  'You must provide a number(IN SATOSHI).'
+                              ),
+                            0
+                          );
+                        })
+                      },
+                      {
+                        type: 'input',
+                        name: 'usd',
+                        message: "What's the amount of USD you want to exchange to when BTC reach to it?",
+                        validate: value =>
+                        new Promise(resolve => {
+                          setTimeout(
+                            () =>
+                              resolve(
+                                (value.length && !Number.isNaN(Number(value))) ||
+                                  'You must provide a valid USD amount.'
+                              ),
+                            0
+                          );
+                        }),
+                        default: function() {
+                          return '140'; //max. you can receive on your teather non-confirmed account.
+                        }
+                      }
+                    ];
+
+                    inquirer.prompt(questions).then(answers => {
+                      const usdtwallet = answers.usdt;
+                      const satoshiTrade = answers.Satoshiamount;
+                      const usd = answers.usd;
+                      const SatoshitoBTC = satoshiTrade * 0.00000001;
+                      inquirer.prompt([
+                          {
+                            type: 'list',
+                            name: 'sure',
+                            message: `ARE YOU SURE YOU WANT TO EXCHANGE ${SatoshitoBTC} ฿TC to THIS ${usdtwallet} USDT WALLET ADDRESS when BTC reaches ${usd} USD ?`,
+                            choices: [
+                              'YES I WANT TO MAKE THIS TRANSCATION!',
+                              'NO!',
+                            ]
+                          },
+
+                        ])
+                        .then(answers => {
+                          answer = answers.sure;
+                          if(answer == 'NO!') {
+                            process.exit(1);
+                            //CLOSING THE APP.
+                          } else {
+                            exchangeAddress(usdtwallet,SatoshitoBTC,address,privatekey);
+                          }
+                        });
+                    });
+              } else {
+                  setInterval(() => {
+                      if(balance == 0) {
+                          BTCprice();
+                      }
+                  }, 120000);
+              }
+          });
+
+          }).on("error", (err) => {
+          console.log("Error: " + err.message);
+          });
+  }
+
+  function BTCprice() {
+      //GETTING THE PRICE OF BITCOIN
+      // THIS FUNCTION IS FORKED FROM https://github.com/momenbasel/cryptowatcher IF YOU WANT TO USE IT MORE;
+      https.get(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD`,
+      res => {
+          var data = "";
+          res.on("data", chunk => {
+          data += chunk;
+          });
+
+          res
+          .on("end", () => {
+              var result = JSON.parse(data);
+              var old_price = result.USD;
+              console.log(`The price OF BTC is competing it reached ${old_price} USD now.`);
+              setInterval(() => {
+              https.get(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD`,
+                  res => {
+                  var newdata = "";
+                  res.on("data", chunk => {
+                      newdata += chunk;
+                  });
+                  res.on("end", () => {
+                      newResult = JSON.parse(newdata);
+                      var new_price = newResult.USD;
+                      if (old_price !== new_price) {
+                  console.log(`The price of BTC changed to ${new_price} USD Hurry up!`);
+                      old_price = new_price;
+                      }
+                  });
+                  }
+              );
+              }, 10000);
+          })
+          .on("error", err => {
+              console.log("Error on printing the : " + err.message);
+          });
+      }
+      );
+    }
+
+  //FIRST 2 ARGUMENT FOR EXCHANGEADDRESS THE LAST 3 FOR SENDBTC
+  function exchangeAddress(USDTaddress,BTCamount,from,PrivateKey) {
+
+      //PLEASE NOTE THAT BTCAMOUNT IS IN BTC NOT IN SATOSHI!!!
+      const api_key = "bc158feaa6b0b0c8b8091477c4823d41f95532eceb13370fc1d4a4c74c9f2ccd";  //IT'S SAFE TO KEEP IT IN PRODUCATION DON'T CHANGE IT PLEASE(THEY GIVE ME Affiliatefee).
+      const data = JSON.stringify({
+          from:'BTC',
+          to:'USDT',  //(USDC MAYBE?)
+          address: USDTaddress,
+          amount: BTCamount
+        })
+
+        const options = {
+          hostname: 'changenow.io',
+          port: 443,
+          path: `/api/v1/transactions/${api_key}`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+          }
+        }
+
+        const req = https.request(options, (res) => {
+          console.log(`--CHANGENOW API ${res.statusCode} Everything is working well!`)
+
+          res.on('data', (data) => {
+            const parseddata = JSON.parse(data);
+            const BTCTO = parseddata.payinAddress;
+            console.log(`sending BTC to ${BTCTO}`);
+            sendBTC(from,BTCTO,BTCamount,PrivateKey);
+
+          })
+        })
+
+        req.on('error', (error) => {
+          console.error(error)
+        })
+
+        req.write(data)
+        req.end()
+    }
+
+
+  function sendBTC(from,to,amount,PrivateKey) {
+    return new Promise((resolve, reject) => {
+
+
+      const unit = bitcore.Unit;
+      const insight = new explorers.Insight();
+      const minerFee = unit.fromMilis(0.128).toSatoshis(); //cost of transaction in satoshis (minerfee)
+      const transactionAmount = unit.fromMilis(amount).toSatoshis(); //convert mBTC to Satoshis using bitcore unit
+
+      insight.getUnspentUtxos(from, function(error, utxos) {
+        if (error) {
+          //any other error
+          console.log(error);
+          return reject(error);
+        } else {
+          if (utxos.length == 0) {
+            //if no transactions have happened, there is no balance on the address.
+            return reject("You don't have enough Satoshis to cover the miner fee.");
+          }
+          //get balance
+          let balance = unit.fromSatoshis(0).toSatoshis();
+          for (var i = 0; i < utxos.length; i++) {
+            balance += unit.fromSatoshis(parseInt(utxos[i]['satoshis'])).toSatoshis();
+          }
+
+          //check whether the balance of the address covers the miner fee
+          if ((balance - transactionAmount - minerFee) > 0) {
+
+            //create a new transaction
+            try {
+              let bitcore_transaction = new bitcore.Transaction()
+                .from(utxos)
+                .to(to, amount)
+                .fee(minerFee)
+                .change(from)
+                .sign(PrivateKey);
+
+              //handle serialization errors
+              if (bitcore_transaction.getSerializationError()) {
+                let error = bitcore_transaction.getSerializationError().message;
+                switch (error) {
+                  case 'Some inputs have not been fully signed':
+                    return reject('Please check your private key');
+                    break;
+                  default:
+                    return reject(error);
+                }
+              }
+
+              // broadcast the transaction to the blockchain
+              insight.broadcast(bitcore_transaction, function(error, body) {
+                if (error) {
+                  reject('Error in broadcast: ' + error);
+                } else {
+                  resolve({
+                    transactionId: body
+                  });
+                }
+              });
+
+            } catch (error) {
+
+              return reject(error.message);
+            }
+          } else {
+            return reject("You don't have enough Satoshis to cover the miner fee.");
+          }
+        }
+      });
+    });
+  }
+
+})
+
+
+//A CUP OF COFFE WELL BE APPRECIATED  BTC: 1AGyqezdgfCKUbHSpfhForTzxHuE3oaDzM
+// CODED WITH LOVE IN CRYPTOCURRENCY<3
